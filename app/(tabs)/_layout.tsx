f57a838b42a71e9c6cf45d736d6264a32b2a165c
Sydney Chiks <112480313+SydaChiks@users.noreply.github.com>
2025-06-17 12:13:17 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, MessageCircle, CreditCard, Newspaper, User } from 'lucide-react-native';
+import { Chrome as Home, MessageCircle, CreditCard, Newspaper, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
