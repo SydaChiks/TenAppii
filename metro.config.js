a614e75f2cc0ebaba630dc4c3a14e123ff5e5066
Sydney Chiks <112480313+SydaChiks@users.noreply.github.com>
2025-06-18 14:35:11 +0200

    Fix Expo Router Setup and Dependencies

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure web support
+config.resolver.platforms = ['ios', 'android', 'native', 'web'];
+
 module.exports = config;
